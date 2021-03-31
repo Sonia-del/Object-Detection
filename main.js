08e83de937a81372e33077bb023f5fb9abcf2398
Sonia-del <66464686+Sonia-del@users.noreply.github.com>
2021-03-31 17:24:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 img = "";
 status = "";
 objects = [];
+len = 0;
 
 function preload()
 {
@@ -9,8 +10,11 @@ function preload()
 
 function setup()
 {
-   canvas = createCanvas(640, 420);
+   canvas = createCanvas(380, 380);
    canvas.center();
+   video = createCapture(VIDEO);
+   video.size(380, 380);
+   video.hide();
    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
@@ -19,7 +23,7 @@ function modelLoaded()
 {
     console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img , gotResult);
+    
 }
 
 function gotResult(error, results)
@@ -30,25 +34,31 @@ function gotResult(error, results)
     }
     console.log(results);
     objects = results;
+    len = results.length;
 }
 
 
 function draw()
 {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
     if(status != "")
+{
+    r = random(255);
+    g = random(255);
+    b = random (255);
+    objectDetector.detect(video , gotResult);
+    for (i = 0; i < len; i++)
     {
-        for (i = 0; i < objects.length; i++)
-        {
-            document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are : " + objects.length;
 
-            fill("#FF0000")
-            percent = floor(objects[i].confidence = 100);
-            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
-            noFill();
-            stroke("#FF0000");
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
+        fill(r, g, b);
+        percent = floor(objects[i].confidence = 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        stroke(r, g, b);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
     }
 }
